ded76f524ca326e7ecd092b4e03392a0f9041c4a
LonaZondani <116520017+LonaZondani@users.noreply.github.com>
2022-11-21 15:59:33 +0200

    Update app.js

--- skiblu website or landing page/app.js
+++ skiblu website or landing page/app.js
@@ -1,9 +1,5 @@
 const automaticTextElement = document.getElementById('automatic-type');
 const wordPhrases = 'Air Service Licence(ASL) and an Operating Certificate(AOC/ROC)?';
-const typingDelay = 200;
-const erasingDelay = 50;
-const repeatText = 200;
-
 
 let i = 0;
 
